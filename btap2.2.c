25768baca4af17b60a2e5a1bb7287d127b675206
HiepTran09101998 <65001490+HiepTran09101998@users.noreply.github.com>
2020-05-08 11:03:44 +0700

    khoi tao
    test thu github

---
+++ btap2.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dLar = 0;
+printf("So Dolar?\n");
+scanf("%d",&dLar);
+float tiGia = 16.666;
+int VND;
+VND = dLar * tiGia;
+printf("So tien sau khi quy doi: %d\n",&VND);
+	return 0;
+}
